725327cb0eb2a620164796db8ab2d31a4452c108
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-06-10 11:26:38 +0100

    Removing unused stuff

--- b2c-app/src/components/index.js
+++ b2c-app/src/components/index.js
@@ -1,11 +1,9 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
-import B2C from './B2C/B2C';
 import HeaderTitle from './HeaderTitle/HeaderTitle';
 import PageNavigation from './PageNavigation/PageNavigation';
 import NavigationLink from './NavigationLink/NavigationLink';
 import Breadcrumbs from './Breadcrumbs/Breadcrumbs';
-import MainContainer from './MainContainer/MainContainer';
 import PageTitle from './PageTitle/PageTitle';
 import FooterNavigation from './FooterNavigation/FooterNavigation';
 import FooterLink from './FooterLink/FooterLink';
@@ -25,12 +23,10 @@ import PageContainer from './PageContainer/PageContainer';
 export default {
     Header: Header,
     Footer: Footer,
-    B2C: B2C,
     HeaderTitle: HeaderTitle,
     PageNavigation: PageNavigation,
     NavigationLink: NavigationLink,
     Breadcrumbs: Breadcrumbs,
-    MainContainer: MainContainer,
     PageTitle: PageTitle,
     FooterNavigation: FooterNavigation,
     FooterLink: FooterLink,
