c13246d1938443268329686b96efe95d17207f98
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-06-12 16:34:55 +0100

    Initial changes, using CSS from NCS, updated header component

--- b2c-app/src/components/Header/Header.js
+++ b2c-app/src/components/Header/Header.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import './Header.scss'
-
 import components from '../../components';
 
 import headerData from '../../data/headerData.json';
@@ -9,11 +7,13 @@ import headerData from '../../data/headerData.json';
 function Header() {
     return (
         <header className="govuk-header " role="banner" data-module="govuk-header">
-            <div className="govuk-header__container govuk-width-container ncs-custom">
-                <div className="govuk-header__content">
-                    <components.HeaderTitle title={headerData.title} />
-                    <components.PageNavigation navigationItems={headerData.navigationItems} />
-                </div>
+            <div className="govuk-header__container govuk-width-container">
+                <components.HeaderTitle title={headerData.title} />
+                TODO see how we make this button work, get JS from govuk-frontend or add that code here to add/remove classes
+                <button type="button" class="govuk-header__menu-button govuk-js-header-toggle" aria-controls="navigation" aria-label="Show or hide Top Level Navigation">Menu</button>
+                <components.PageNavigation>
+                    {headerData.navigationItems}
+                </components.PageNavigation>
             </div>
         </header>
     )
--- b2c-app/src/components/PageNavigation/PageNavigation.js
+++ b2c-app/src/components/PageNavigation/PageNavigation.js
@@ -5,12 +5,12 @@ import components from '../../components';
 function PageNavigation(props) {
 
     //build array of components with actual config
-    const navItemComponents = props.navigationItems.map( 
+    const navItemComponents = props.children.map(
         navItem => {
-            return <components.NavigationLink title={navItem.title} link={navItem.link} key={navItem.title+navItem.link}/> 
+            return <components.NavigationLink title={navItem.title} link={navItem.link} key={navItem.title + navItem.link} />
         }
     );
-    
+
     return (
         <nav>
             <ul id="navigation" className="govuk-header__navigation " aria-label="Top Level Navigation">
