742ae2d2400e1051ba29f910d468f71a1e27f89e
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-06-15 11:13:30 +0100

    Comment turned into an actual comment

--- b2c-app/src/components/Header/Header.js
+++ b2c-app/src/components/Header/Header.js
@@ -9,7 +9,7 @@ function Header() {
         <header className="govuk-header " role="banner" data-module="govuk-header">
             <div className="govuk-header__container govuk-width-container">
                 <components.HeaderTitle title={headerData.title} />
-                TODO see how we make this button work, get JS from govuk-frontend or add that code here to add/remove classes
+                {/* TODO see how we make this button work, get JS from govuk-frontend or add that code here to add/remove classes */}
                 <button type="button" class="govuk-header__menu-button govuk-js-header-toggle" aria-controls="navigation" aria-label="Show or hide Top Level Navigation">Menu</button>
                 <components.PageNavigation>
                     {headerData.navigationItems}
