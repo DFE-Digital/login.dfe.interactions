3f38988e00b64c40eb0fbb0d4859b1d26704e07b
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-06-22 12:53:47 +0100

    Undo babel plugin addition

--- src/app/b2c/index.js
+++ src/app/b2c/index.js
@@ -2,8 +2,7 @@ require('ignore-styles');
 
 require('@babel/register')({
     ignore: [/(node_modules)/],
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/transform-runtime']
+    presets: ['@babel/preset-env', '@babel/preset-react']
 });
 
 const router = require('../../../b2c-app/src/server');
