471d6f99442b2841dca50b21d0027f0ab8773968
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-06-22 08:32:27 +0100

    Updated policy names for TEST

--- b2c-app/src/constants/policies.js
+++ b2c-app/src/constants/policies.js
@@ -1,10 +1,10 @@
 export const POLICIES = {
-    SIGNIN_INVITATION: 'B2C_1A_signin_invitation',
-    ACCOUNT_SIGNUP: 'B2C_1A_account_signup',
-    PASSWORD_RESET: 'B2C_1A_passwordreset',
-    PASSWORD_RESET_CONFIRMATION: 'B2C_1A_passwordResetConformation',
-    FIND_EMAIL: 'B2C_1A_findEmail',
-    SIGNUP_CONFIRMATION: 'B2C_1A_signup_confirmation',
-    SIGNUP_INVITATION: 'B2C_1A_signup_invitation',
-    CHANGE_EMAIL: 'B2C_1A_changeemail'
+    SIGNIN_INVITATION: 'B2C_1A_Signin_Invitation',
+    ACCOUNT_SIGNUP: 'B2C_1A_Account_Signup',
+    PASSWORD_RESET: 'B2C_1A_Password_Reset',
+    PASSWORD_RESET_CONFIRMATION: 'B2C_1A_Password_Reset_Confirmation',
+    FIND_EMAIL: 'B2C_1A_Find_Email',
+    SIGNUP_CONFIRMATION: 'B2C_1A_Signup_Confirmation',
+    SIGNUP_INVITATION: 'B2C_1A_Signup_Invitation',
+    CHANGE_EMAIL: 'B2C_1A_Change_Email'
 };
