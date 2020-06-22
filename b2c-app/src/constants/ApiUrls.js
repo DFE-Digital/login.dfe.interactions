1e34a3a1346eed36450bf80ad4cc5f4a9fd42238
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-06-22 17:01:53 +0100

    Fixed url that has changed

--- b2c-app/src/constants/ApiUrls.js
+++ b2c-app/src/constants/ApiUrls.js
@@ -1,4 +1,4 @@
 export const API_URLS = {
     //TODO replace this URL with placeholder __--changeEmailAPI--__ when env vars are ready
-    CHANGE_EMAIL: "https://dev-api-customerengagement.platform.education.gov.uk/ncsidentity/ChangeEmail"
+    CHANGE_EMAIL: "https://dev-api-customerengagement.platform.education.gov.uk/authncs/ChangeEmail"
 };
