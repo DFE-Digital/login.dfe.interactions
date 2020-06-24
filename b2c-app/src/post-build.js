cbbcf8a05f301f7d12a0acd11f80a95b94ee27fd
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-06-24 10:36:42 +0100

    Removed replacements in post-build script

--- b2c-app/src/post-build.js
+++ b2c-app/src/post-build.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const fs = require('fs');
-var glob = require("glob")
 
 let filename = `${process.argv[2]}/index.html`;
 
@@ -11,7 +10,6 @@ fs.readFile(indexFile, 'utf8', (err, data) => {
     }
 
     let newFile = data.replace(/rel="stylesheet"/g, 'rel="stylesheet" data-preload="true"');
-    newFile = newFile.replace(/\/__--b2cPath--__/g, process.env.B2C_ROOT_PATH);
 
     fs.writeFile(filename, newFile, (err) => {
         if (err) {
@@ -19,22 +17,3 @@ fs.readFile(indexFile, 'utf8', (err, data) => {
         }
     });
 });
-
-glob(`${process.argv[2]}/static/js/main.*.chunk.js`, null, function (er, files) {
-    files.forEach((filename) => {
-        const mainJsFile = path.resolve(filename);
-        fs.readFile(mainJsFile, 'utf8', (err, data) => {
-            if (err) {
-                console.error('Something went wrong:', err);
-            }
-
-            let newFile = data.replace(/__--changeEmailAPI--__/g, process.env.B2C_CHANGE_EMAIL_ENDPOINT);
-
-            fs.writeFile(filename, newFile, (err) => {
-                if (err) {
-                    console.error(err);
-                }
-            });
-        });
-    })
-})
