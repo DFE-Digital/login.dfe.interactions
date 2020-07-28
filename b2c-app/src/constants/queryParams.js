83bfd37c395a65b2d63d7079a57a0377458c59fa
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-07-28 16:05:50 +0100

    Fix for links

--- b2c-app/src/constants/queryParams.js
+++ b2c-app/src/constants/queryParams.js
@@ -2,7 +2,7 @@ export const QUERY_PARAMS = {
     CLIENT_ID: 'client_id',
     REDIRECT_URI: 'redirect_uri',
     ID_TOKEN_HINT: 'id_token_hint',
-    TENANT_ID: 'tenand_id',
+    TENANT_ID: 'tenant_id',
     POLICY: 'policy',
     PAGE: 'custom_ui_page'
 }
