eb241f13fecd66851f7b0fc066541526fc5f79e4
Jose Luis Garcia Ramos <joseluisgraa@gmail.com>
2020-08-14 16:53:50 +0100

    updated query param to get id_token_hint

--- b2c-app/src/constants/queryParams.js
+++ b2c-app/src/constants/queryParams.js
@@ -1,7 +1,7 @@
 export const QUERY_PARAMS = {
     CLIENT_ID: 'client_id',
     REDIRECT_URI: 'redirect_uri',
-    ID_TOKEN_HINT: 'idtokenhint',
+    ID_TOKEN_HINT: 'id_token_hint',
     TENANT_ID: 'tenant_id',
     POLICY: 'policy',
     PAGE: 'custom_ui_page',
