cfd73b456bdbb4e5dc412829c815e9211bfde594
Steve Coleman-Williams <steve@archronos.com>
2020-09-04 13:40:33 +0100

    cert hack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const listEndpoints = require('express-list-endpoints');
 const express = require('express');
