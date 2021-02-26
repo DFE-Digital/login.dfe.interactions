1d5f67236e32faf13e9308904ffd227f868a109d
Matt Cavanagh <matt.c@darkynt.co.uk>
2021-02-26 14:33:48 +0000

    Adds a debug log

--- src/app/InteractionComplete/index.js
+++ src/app/InteractionComplete/index.js
@@ -1,4 +1,5 @@
 const Config = require('./../../infrastructure/Config')();
+const logger = require('../../../../login.dfe.oidc/src/infrastructure/logger');
 const { sendResult } = require('./../../infrastructure/utils');
 const { signData } = require('./../../infrastructure/utils');
 
@@ -22,13 +23,13 @@ class InteractionComplete {
   static getPostbackDetails(uuid, data) {
     const postbackData = { uuid };
     const validateUser = { uuid, uid: data.uid };
-
     if (data !== null) {
       Object.keys(data).forEach((key) => {
         postbackData[key] = data[key];
       });
     }
-
+    logger.info('About to sign signature and sSignature');
+    logger.debug(`postBack Data is + ${JSON.stringify(postbackData)}`);
     postbackData.sig = signData(postbackData);
     postbackData.sSig = signData(validateUser);
 
@@ -40,7 +41,7 @@ class InteractionComplete {
 
   static process(uuid, data, req, res) {
     const postbackDetails = InteractionComplete.getPostbackDetails(uuid, data);
-
+    logger.info('Process after interaction Complete')
     sendResult(req, res, 'InteractionComplete/views/index', {
       destination: postbackDetails.destination,
       postbackData: postbackDetails.data,
